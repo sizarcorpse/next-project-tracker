507b7ecf4e0597bfd74ad156d494f8dd653e50b1
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-12 22:13:10 +0600

    add(action): added getCurrentUser to fetch current user data from database

---
+++ src/actions/getCurrentUser.ts
@@ -0,0 +1,31 @@
+import { authOptions } from "@/libs/auth";
+import { prisma } from "@/libs/prisma";
+import { getServerSession } from "next-auth/next";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (error: any) {
+    return null;
+  }
+}
