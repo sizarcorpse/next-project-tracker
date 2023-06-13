d434b32f65ef4849526a005d6e00579da501abe1
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-14 01:25:30 +0600

    update(prisma-schema): update Profile model

---
+++ prisma/migrations/20230613161256_v3/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Profile" ADD COLUMN     "company" TEXT,
+ADD COLUMN     "contact" TEXT,
+ADD COLUMN     "designation" TEXT,
+ADD COLUMN     "facebook" TEXT,
+ADD COLUMN     "gender" TEXT,
+ADD COLUMN     "github" TEXT,
+ADD COLUMN     "linkedin" TEXT,
+ADD COLUMN     "location" TEXT,
+ADD COLUMN     "twitter" TEXT,
+ADD COLUMN     "website" TEXT;
