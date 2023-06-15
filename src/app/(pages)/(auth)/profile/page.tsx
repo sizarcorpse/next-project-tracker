36e0beb885fd0b749eeaa5133671d9c8447a917b
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-15 22:09:32 +0600

    wip(profile-page): added user only protection

--- src/app/(pages)/(auth)/profile/page.tsx
+++ src/app/(pages)/(auth)/profile/page.tsx
@@ -1,7 +1,6 @@
 import { getCurrentUser } from "@/actions/";
 import { UserInformation } from "@/components/profile/";
 import { redirect } from "next/navigation";
-
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="px-4 my-8 sm:px-6 md:px-8 lg:px-16 xl:px-20 mx-auto max-w-screen-2xl">
@@ -32,9 +31,7 @@ const ProfilePage = async () => {
             }}
           />
         </div>
-        <div className="basis-full flex flex-col gap-4 items-start justify-start bg-slate-100 p-2 rounded-md md:basis-[calc(100%-296px-32px)]">
-          {/* {JSON.stringify(user, null, 2)} */}
-        </div>
+        <div className="basis-full flex flex-col gap-4 items-start justify-start bg-slate-100 p-4 rounded-md md:basis-[calc(100%-296px-32px)]"></div>
       </div>
     </Container>
   );
