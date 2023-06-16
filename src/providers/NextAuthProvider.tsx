e90b3b5006afffb6dbda12d9ee41bb8d2ce27022
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-17 02:04:44 +0600

    update(auth-provider): added SWRConfig provider

--- src/providers/NextAuthProvider.tsx
+++ src/providers/NextAuthProvider.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-
+import { SWRConfig } from "swr";
 type Props = {
   children?: React.ReactNode;
 };
 
 export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <SessionProvider>{children}</SessionProvider>
+    </SWRConfig>
+  );
 };
