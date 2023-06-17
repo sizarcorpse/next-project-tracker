18aa0a12252ddbbb5ef7e53ccdf19b7af0d0f802
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-17 23:33:54 +0600

    add(profile-component): added UpdateProfileButton for goto profile update page

---
+++ src/components/profile/UpdateProfileButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+import { RiEditLine } from "react-icons/ri";
+
+const UpdateProfileButton: React.FC = () => {
+  return (
+    <Link
+      href="/profile/update"
+      className="group flex flex-row items-center justify-center gap-2 bg-slate-200 text-slate-800 px-3 py-2 rounded-xl text-xs font-semibold"
+    >
+      <span>Update Profile</span>
+      <RiEditLine
+        size="1rem"
+        className="group-hover:text-amber-500 transition-colors"
+      />
+    </Link>
+  );
+};
+
+export default UpdateProfileButton;
