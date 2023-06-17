23ecafb8288a5d73f4d263edae6d7b5146119d8f
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-17 18:20:50 +0600

    add(z-profile): added validation for update profile model

--- src/validators/profile.ts
+++ src/validators/profile.ts
@@ -16,6 +16,9 @@ const instagram =
 
 const website = /^(https?:\/\/)?[a-z0-9-]+(\.[a-z0-9-]+)+([/?].*)?$/i;
 
+const email =
+  /^[a-zA-Z0-9._%+-]+@(gmail|outlook|hotmail|yahoo|proton)\.[a-zA-Z]{2,}$/i;
+
 export const ProfileValidator = z.object({
   designation: z
     .string()
@@ -34,14 +37,16 @@ export const ProfileValidator = z.object({
     .regex(website, {
       message: "Website must be a valid URL",
     })
-    .optional(),
+    .or(z.string().nullable().optional()),
   location: z
     .string()
     .max(50, {
       message: "Location must be at most 50 characters long",
     })
     .optional(),
-  gender: z.enum(["Male", "Female", "Other"]).optional(),
+  gender: z
+    .enum(["Male", "Female", "Other"])
+    .or(z.string().nullable().optional()),
   bio: z
     .string()
     .max(500, {
@@ -50,41 +55,40 @@ export const ProfileValidator = z.object({
     .optional(),
   contact: z
     .string()
-    .email({
+    .regex(email, {
       message: "Contact must be a valid email",
     })
-    .optional(),
+    .or(z.string().nullable().optional()),
   linkedin: z
     .string()
     .regex(linkedin, {
       message: "Linkedin must be a valid URL",
     })
-    .optional(),
+    .or(z.string().nullable().optional()),
   github: z
     .string()
     .regex(github, {
       message: "Github must be a valid URL",
     })
-    .optional(),
-
+    .or(z.string().nullable().optional()),
   twitter: z
     .string()
     .regex(twitter, {
       message: "Twitter must be a valid URL",
     })
-    .optional(),
+    .or(z.string().nullable().optional()),
   facebook: z
     .string()
     .regex(facebook, {
       message: "Facebook must be a valid URL",
     })
-    .optional(),
+    .or(z.string().nullable().optional()),
   instagram: z
     .string()
     .regex(instagram, {
       message: "Instagram must be a valid URL",
     })
-    .optional(),
+    .or(z.string().nullable().optional()),
 });
 
 export type ProfileUpdateRequest = z.infer<typeof ProfileValidator>;
