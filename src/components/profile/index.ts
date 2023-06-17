4973db276964389e5acd0d7d929e92c439906900
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-17 23:45:00 +0600

    added indexes

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -1,2 +1,6 @@
+export { default as LoadingDataRipple } from "./LoadingDataRipple";
 export { default as ProfilePhoto } from "./ProfilePhoto";
+export { default as UpdateProfileButton } from "./UpdateProfileButton";
+export { default as UserContact } from "./UserContact";
 export { default as UserInformation } from "./UserInformation";
+export { default as UserSocials } from "./UserSocials";
