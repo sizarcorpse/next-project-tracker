a6f3654ff6692fbfb93a95bf55580791fe66ed77
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-17 23:41:56 +0600

    refactor(profile-page): Pass user to UserInformation

--- src/app/(pages)/(auth)/profile/page.tsx
+++ src/app/(pages)/(auth)/profile/page.tsx
@@ -1,6 +1,7 @@
 import { getCurrentUser } from "@/actions/";
 import { UserInformation } from "@/components/profile/";
 import { redirect } from "next/navigation";
+
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="px-4 my-8 sm:px-6 md:px-8 lg:px-16 xl:px-20 mx-auto max-w-screen-2xl">
@@ -15,21 +16,11 @@ const ProfilePage = async () => {
     redirect("/signin?callbackUrl=/profile");
   }
 
-  const { image, name, username, role, email } = user || {};
-
   return (
     <Container>
       <div className="flex flex-row items-start justify-between flex-wrap gap-8 md:flex-nowrap">
         <div className="basis-full flex flex-col gap-6 items-start justify-start bg-slate-100 p-4 rounded-md md:basis-[296px]">
-          <UserInformation
-            data={{
-              name,
-              username,
-              role,
-              image,
-              email,
-            }}
-          />
+          <UserInformation user={user} />
         </div>
         <div className="basis-full flex flex-col gap-4 items-start justify-start bg-slate-100 p-4 rounded-md md:basis-[calc(100%-296px-32px)]"></div>
       </div>
