9be43d0673bb86a8849e05c3e9899928f5ea6ef9
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-18 19:30:52 +0600

    update(next-config): added new variable

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   env: {
     GITHUB_ID: process.env.GITHUB_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
+    NEXT_API_URL: process.env.NEXT_API_URL || "http://localhost:3000/api",
   },
   images: {
     domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
