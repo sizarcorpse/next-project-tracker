61aff1327cc59d0a5290c896867e666f7ccd4f5f
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-19 23:02:34 +0600

    update(user-rotue): update, refactor PATCH req and added change password for credential provider users

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,5 +1,6 @@
 import { authOptions } from "@/libs/auth";
 import { prisma } from "@/libs/prisma";
+import { compare, hash } from "bcryptjs";
 import { getServerSession } from "next-auth/next";
 import { NextResponse } from "next/server";
 
@@ -75,43 +76,83 @@ export async function PATCH(request: Request) {
       );
     }
 
+    const data: any = {};
     const body = await request.json();
-    const { name, image, email, username } = body;
+    const { name, email, username, currentPassword, newPassword } = body;
 
-    const userExists = await prisma.user.findFirst({
-      where: {
-        OR: [
-          {
-            username:
-              session?.user?.username === username ? undefined : username,
-          },
-          {
-            email: session?.user?.email === email ? undefined : email,
-          },
-        ],
-      },
-    });
+    if (name !== session?.user?.name) {
+      data["name"] = name;
+    }
 
-    if (userExists) {
-      return new NextResponse(
-        JSON.stringify({
-          status: "error",
-          message: "Username or email is already taken",
-        }),
-        { status: 400 }
+    if (email !== session?.user?.email || username !== session?.user.username) {
+      const um = session?.user?.username === username ? undefined : username;
+      const em = session?.user?.email === email ? undefined : email;
+
+      const IsUserExists = await prisma.user.findFirst({
+        where: {
+          OR: [
+            {
+              username: um,
+            },
+            {
+              email: em,
+            },
+          ],
+        },
+      });
+
+      if (IsUserExists) {
+        return new NextResponse(
+          JSON.stringify({
+            status: "error",
+            message: "Username or email is already taken",
+          }),
+          { status: 400 }
+        );
+      } else {
+        data["username"] = um;
+        data["email"] = em;
+      }
+    }
+
+    if (currentPassword && newPassword) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: session?.user?.id,
+        },
+        select: {
+          password: true,
+          accounts: true,
+        },
+      });
+
+      const isPasswordMatch = await compare(
+        currentPassword,
+        user?.password as string
       );
+
+      if (
+        isPasswordMatch &&
+        Object.keys(user?.accounts as object).length === 0
+      ) {
+        const hashedPassword = await hash(newPassword, 12);
+        data["password"] = hashedPassword;
+      } else {
+        return new NextResponse(
+          JSON.stringify({
+            status: "error",
+            message: "Password is incorrect",
+          }),
+          { status: 400 }
+        );
+      }
     }
 
     const user = await prisma.user.update({
       where: {
         id: session?.user?.id,
       },
-      data: {
-        name,
-        email,
-        username,
-        image,
-      },
+      data: data,
       select: {
         id: true,
         name: true,
@@ -124,7 +165,7 @@ export async function PATCH(request: Request) {
     return new NextResponse(
       JSON.stringify({
         status: "ok",
-        data: user,
+        data: "user",
       }),
       { status: 200 }
     );
