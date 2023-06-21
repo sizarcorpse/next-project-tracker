ac00fcf992e07b29b4c3ad36f4174023c45a53f4
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-22 01:30:42 +0600

    add(landing-page): added new landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,42 @@
-export default async function Home() {
+const about = {
+  packages: [
+    "NextJS",
+    "NextAuth",
+    "TailwindCSS",
+    "TypeScript",
+    "Prisma",
+    "PostgreSQL",
+    "React Hook Form",
+    "React-hot-toast",
+    "Swr",
+    "React-icons",
+    "Zod",
+  ],
+};
+
+const Home = async () => {
+  const { packages } = about;
   return (
-    <>
-      <section className="bg-ct-blue-600 min-h-screen pt-20">
-        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-          <p className="text-3xl font-semibold">
-            Welcome to NextAuth with Prisma ORM and Next.js 13
-          </p>
+    <main className="flex items-center justify-center p-6">
+      <div className="container max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <h1 className="text-xl font-bold text-slate-800 uppercase md:text-4xl lg:text-6xl text-center">
+            Next Auth Boilerplate
+          </h1>
+          <div className="flex flex-row items-center justify-center flex-wrap gap-2">
+            {packages.map((item, index) => (
+              <p
+                key={index}
+                className="px-2 py-1 rounded-lg border border-slate-300 text-sm text-slate-800 font-semibold cursor-pointer hover:bg-amber-300 hover:border-rose-400"
+              >
+                {item}
+              </p>
+            ))}
+          </div>
         </div>
-      </section>
-    </>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
