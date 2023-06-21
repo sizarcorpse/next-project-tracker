db7e40a206e0915609cede3bf488b2c37ffee165
Sizar Corpse <sizarcorpse@gmail.com>
2023-06-21 23:43:12 +0600

    add index

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,3 +1,4 @@
 export { default as GuestMenu } from "./GuestMenu";
 export { default as NavigationBar } from "./NavigationBar";
+export { default as SettingsMenu } from "./SettingsMenu";
 export { default as UserMenu } from "./UserMenu";
