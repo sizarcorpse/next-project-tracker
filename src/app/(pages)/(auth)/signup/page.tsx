6cbe97bf4655d2800db3d06a1c4648b284fb71a3
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-06 22:12:48 +0600

    update(pages): update odl tailwind classws to match new the me classes

--- src/app/(pages)/(auth)/signup/page.tsx
+++ src/app/(pages)/(auth)/signup/page.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 const Divider = () => {
   return (
     <div className="flex items-center justify-center gap-4 my-8">
-      <div className="w-1/2 h-[0.5px] bg-zinc-400"></div>
-      <p className="text-sm text-slate-200">or</p>
-      <div className="w-1/2 h-[0.5px]  bg-zinc-400"></div>
+      <div className="w-1/2 h-[0.5px] bg-primary/50"></div>
+      <p className="text-sm text-primary">or</p>
+      <div className="w-1/2 h-[0.5px] bg-primary/50"></div>
     </div>
   );
 };
@@ -16,10 +16,10 @@ const Divider = () => {
 const SignUpPage = () => {
   return (
     <main className="w-full flex items-center justify-center p-0 md:p-6">
-      <div className="container max-w-screen-lg flex flex-row items-center justify-center flex-wrap mx-auto bg-slate-900 rounded-none md:rounded-lg">
+      <div className="container max-w-screen-lg flex flex-row items-center justify-center flex-wrap mx-auto bg-card rounded-none px-4 md:rounded-lg">
         <div className="grow-1 shrink-1 basis-full order-2 px-4 py-8 md:basis-1/2 md:order-1 md:p-6 lg:p-16">
           <div className="mb-4 flex flex-col gap-4">
-            <h2 className="text-2xl font-bold text-slate-100 mb-4">
+            <h2 className="text-2xl font-bold text-primary mb-4">
               Create an account
             </h2>
             <LoginWithGithub />
@@ -27,12 +27,12 @@ const SignUpPage = () => {
           <Divider />
           <div>
             <div className="mb-6">
-              <h2 className="text-base font-semibold text-slate-100">
+              <h2 className="text-base font-semibold text-primary">
                 Sign up with email
               </h2>
-              <span className="text-sm text-slate-200 ">
+              <span className="text-sm text-primary">
                 Already have an account?{" "}
-                <Link href="/signin" className="text-amber-300">
+                <Link href="/signin" className="text-muted-foreground">
                   Sign in
                 </Link>
               </span>
@@ -40,7 +40,7 @@ const SignUpPage = () => {
             <SignUpForm />
           </div>
         </div>
-        <div className="grow-1 shrink-1 basis-1/1 order-1 md:basis-1/2 md:order-2 md:p-4">
+        <div className="grow-1 shrink-1 basis-1/1 order-1 md:basis-1/2 md:order-2 p-0 md:py-4">
           <Image
             src="https://images.unsplash.com/photo-1604871000636-074fa5117945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
             alt="Sign up"
@@ -49,7 +49,7 @@ const SignUpPage = () => {
             style={{
               objectFit: "cover",
             }}
-            className="rounded-b-lg aspect-square md:aspect-auto md:rounded-lg xl:aspect-auto"
+            className="rounded-b-lg aspect-square sm:aspect-video md:aspect-auto md:rounded-lg xl:aspect-auto"
           />
         </div>
       </div>
