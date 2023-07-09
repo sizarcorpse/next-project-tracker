e24fb8895e5eec90d51e293afffd314dda7a5f25
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-09 23:46:28 +0600

    add(technology-component): added a resuable dialog CreateTechnology to create and update

---
+++ src/components/technologies/CreateTechnology.tsx
@@ -0,0 +1,34 @@
+import { CreateTechnologyForm } from "@/components/technologies/";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+
+const CreateTechnology = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className="px-3 py-0">
+          <Plus className="h-4 w-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-screen-md min-h-fit flex flex-col items-start justify-start gap-6 p-4">
+        <DialogHeader className="w-full">
+          <DialogTitle className="text-xl">Create Technology</DialogTitle>
+          <DialogDescription className="text-sm">
+            Create a new Technology here
+          </DialogDescription>
+        </DialogHeader>
+        <CreateTechnologyForm />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateTechnology;
