fecf9decbf03beb02b6085dd19a850d7e9c2cc6b
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-09 23:50:09 +0600

    added indexes

---
+++ src/components/technologies/index.ts
@@ -0,0 +1,4 @@
+export { default as CreateTechnology } from "./CreateTechnology";
+export { default as CreateTechnologyForm } from "./CreateTechnologyForm";
+export { default as TechnologiesOverview } from "./TechnologiesOverview";
+export { default as TechnologyCard } from "./TechnologyCard";
