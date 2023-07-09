5a1b886505ae1bdb2d2b8fad9f2ad3d4a9a6e349
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-09 23:48:30 +0600

    add(technology-component): added TechnologiesOverview to show all the technology used in a project

---
+++ src/components/technologies/TechnologiesOverview.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { CreateTechnology, TechnologyCard } from "@/components/technologies";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ChevronRight } from "lucide-react";
+import useSWR from "swr";
+
+const TechnologiesOverview = () => {
+  const { data, error } = useSWR(`${process.env.NEXT_API_URL}/technologies`, {
+    revalidateOnFocus: false,
+    revalidateOnMount: true,
+    revalidateOnReconnect: false,
+    revalidateIfStale: false,
+  });
+
+  const { technologies, total } = data?.data || {
+    technologies: [],
+    total: 0,
+  };
+
+  return (
+    <Card className="flex flex-col items-start justify-start gap-6 p-4">
+      <CardHeader className="p-0 w-full">
+        <div className="flex flex-row justify-between gap-4">
+          <div>
+            <CardTitle className="text-xl">Technologies</CardTitle>
+            <CardDescription className="text-sm">
+              Manage your Technologies here
+            </CardDescription>
+          </div>
+          <CreateTechnology />
+        </div>
+      </CardHeader>
+      <CardContent className="p-0">
+        <ScrollArea className="h-44">
+          <div className="pr-4 w-full flex flex-row items-start justify-start gap-2 flex-wrap">
+            {technologies &&
+              technologies.map((item: any, index: any) => (
+                <TechnologyCard key={index} item={item} />
+              ))}
+          </div>
+        </ScrollArea>
+      </CardContent>
+      <CardFooter className="p-0 w-full flex items-center justify-end">
+        <Button variant="ghost" className="text-sm p-2">
+          Discover All <ChevronRight className="ml-2 w-4 h-4" />
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default TechnologiesOverview;
