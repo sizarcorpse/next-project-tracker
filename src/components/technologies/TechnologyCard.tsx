805325825f0dec45b6c37f4b11518aa8a22c4b8e
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-09 23:47:23 +0600

    add(technology-component): added TechnologyCard component to show details of a technology

---
+++ src/components/technologies/TechnologyCard.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Svg } from "@/components/global";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
+
+type TechnologyCardProps = {
+  item: {
+    name: string;
+    icon: string;
+    slug: string;
+  };
+};
+
+const TechnologyCard: FC<TechnologyCardProps> = ({ item }) => {
+  const { name, icon, slug } = item;
+  const router = useRouter();
+  return (
+    <Card
+      className="group flex flex-row items-center py-2 px-4 gap-2 cursor-pointer hover:bg-primary hover:text-primary-foreground"
+      onClick={() => router.push(`/technology/${slug}`)}
+    >
+      <Svg
+        className="w-4 h-4 fill-card-foreground group-hover:fill-primary-foreground"
+        url={icon}
+      />
+      <CardTitle className="text-sm font-normal">{name}</CardTitle>
+    </Card>
+  );
+};
+
+export default TechnologyCard;
