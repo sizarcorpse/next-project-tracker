a56adbfe5f53014ebfad032b4f69ef7707c0b02f
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-11 02:07:23 +0600

    wip(home-page): in developing stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { TeamOverview } from "@/components/team";
 import { TechnologiesOverview } from "@/components/technologies";
+import { Button } from "@/components/ui/button";
 
 const Home = async () => {
   return (
@@ -6,7 +8,9 @@ const Home = async () => {
       <div className="container max-w-screen-2xl mx-auto p-6 min-h-[calc(100vh-52px-16px)] grid grid-cols-12 gap-4">
         <div className="col-span-8 rounded-md h-full bg-muted">?</div>
         <div className="col-span-4 grid grid-cols-1 grid-rows-12 gap-4 ">
-          <div className="bg-muted rounded-md row-span-2">Members</div>
+          <div className="row-span-2">
+            <TeamOverview />
+          </div>
           <div className="row-span-5">
             <TechnologiesOverview />
           </div>
