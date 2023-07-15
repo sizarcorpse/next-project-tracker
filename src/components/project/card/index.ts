f2a70a8340af38aef244971f4f764a634b1ce14a
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-16 02:38:09 +0600

    add index

---
+++ src/components/project/card/index.ts
@@ -0,0 +1,10 @@
+export { default as ProjectDeadlineDisplay } from "./ProjectDeadlineDisplay";
+export { default as ProjectDropdownActions } from "./ProjectDropdownActions";
+export { default as ProjectLinksDisplay } from "./ProjectLinksDisplay";
+export { default as ProjectPriorityDisplay } from "./ProjectPriorityDisplay";
+export { default as ProjectStageDisplay } from "./ProjectStageDisplay";
+export { default as ProjectStatusDisplay } from "./ProjectStatusDisplay";
+export { default as ProjectTagsDisplay } from "./ProjectTagsDisplay";
+export { default as ProjectTitleDisplay } from "./ProjectTitleDisplay";
+export { default as ProjectTypeDisplay } from "./ProjectTypeDisplay";
+export { default as ProjectVisibilityDisplay } from "./ProjectVisibilityDisplay";
