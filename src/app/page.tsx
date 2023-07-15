22304c46e5f34e74cd30dde8714c9040acd7a19f
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-16 02:40:58 +0600

    wip(home-page): in developing stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,25 @@
+import { CreateNewProject, ProjectsCollection } from "@/components/project";
 import { TagOverview } from "@/components/tag";
 import { TeamOverview } from "@/components/team";
 import { TechnologiesOverview } from "@/components/technologies";
-
 const Home = async () => {
   return (
     <main className="flex flex-col gap-6 items-center justify-center">
-      <div className="container max-w-screen-2xl mx-auto p-6 min-h-[calc(100vh-52px-16px)] grid grid-cols-12 gap-4">
-        <div className="col-span-12 md:col-span-8 rounded-md h-full bg-muted">
-          ?
+      <div className="container max-w-screen-2xl mx-auto p-6 grid grid-cols-12 gap-4">
+        <div className="col-span-12 rounded-md h-full bg-muted p-4 gap-4 md:col-span-8 flex flex-col items-start justify-start">
+          <div className="grid grid-cols-3">
+            <CreateNewProject variant="card" />
+          </div>
+          <ProjectsCollection />
         </div>
-        <div className="col-span-12 md:col-span-4 grid grid-cols-1 grid-rows-3 gap-6">
-          <div className="row-span-3">
+        <div className="col-span-12 md:col-span-4 grid grid-cols-1 content-start gap-6">
+          <div className="">
             <TeamOverview />
           </div>
-          <div className="row-span-6">
+          <div className="">
             <TechnologiesOverview />
           </div>
-          <div className="row-span-3">
+          <div className="">
             <TagOverview />
           </div>
         </div>
