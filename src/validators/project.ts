9e6d20a92bae4204cba9b1aa07a00f0711a51d1d
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-19 17:38:20 +0600

    update(z-project): added technologies

--- src/validators/project.ts
+++ src/validators/project.ts
@@ -1,5 +1,11 @@
 import * as z from "zod";
 
+const technologySchema = z.object({
+  value: z.string(),
+  label: z.string(),
+  icon: z.string().optional(),
+});
+
 const projectSchema = z.object({
   title: z.string().min(3, "Name should be at least 3 characters"),
   description: z.string().optional(),
@@ -12,6 +18,7 @@ const projectSchema = z.object({
   githubLink: z.string().url().or(z.string().nullable()),
   devLink: z.string().url().or(z.string().nullable()),
   liveLink: z.string().url().or(z.string().nullable()),
+  technologies: z.array(technologySchema).optional(),
 });
 
 export default projectSchema;
