2b23247dbfe58ed2129d3195dc4e50d2c49fef6f
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-19 17:40:48 +0600

    wip(project-page): update ProjectDetailsCard

--- src/app/(pages)/(project)/projects/[project]/page.tsx
+++ src/app/(pages)/(project)/projects/[project]/page.tsx
@@ -1,11 +1,12 @@
-import { ProjectDetailsCard } from "@/components/project/";
+import { ProjectDetailsCard, ProjectTagsCard } from "@/components/project/";
 
-const ProjectPage = async ({ params: { project } }: any) => {
+const ProjectPage = async () => {
   return (
     <main className="flex flex-col gap-6 items-center justify-center">
       <div className="container max-w-screen-2xl mx-auto p-6 min-h-[calc(100vh-52px-16px)] grid grid-cols-12 gap-4">
-        <div className="col-span-12 md:col-span-4 flex flex-col items-start justify-start">
+        <div className="col-span-12 md:col-span-4 flex flex-col items-start justify-star gap-6">
           <ProjectDetailsCard />
+          <ProjectTagsCard />
         </div>
         <div className="col-span-12 md:col-span-8 grid grid-cols-1 grid-rows-3 gap-6">
           <div className="row-span-3"></div>
