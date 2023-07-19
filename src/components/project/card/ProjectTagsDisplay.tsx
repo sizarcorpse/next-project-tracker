c21adb73690c98f1d01fbf06944a25f8763c3e01
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-19 17:59:47 +0600

    fix(project-comp): minor fix

--- src/components/project/card/ProjectTagsDisplay.tsx
+++ src/components/project/card/ProjectTagsDisplay.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { Badge } from "@/components/ui/badge";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const ProjectTagsDisplay = ({ tags }: any) => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-row items-center justify-start gap-1">
+    <div className="flex flex-row flex-wrap items-center justify-start gap-1">
       {tags.map((tag: any) => (
         <Badge
           key={tag.id}
