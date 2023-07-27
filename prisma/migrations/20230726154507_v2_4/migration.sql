6a61e4e6a79d07a535a1407c80db8220499d87ad
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-27 21:52:11 +0600

    added migration

---
+++ prisma/migrations/20230726154507_v2_4/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_ProjectMembers" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ProjectMembers_AB_unique" ON "_ProjectMembers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ProjectMembers_B_index" ON "_ProjectMembers"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ProjectMembers" ADD CONSTRAINT "_ProjectMembers_A_fkey" FOREIGN KEY ("A") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProjectMembers" ADD CONSTRAINT "_ProjectMembers_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
