8fe0975f0e7878ccf4851c86162843e3a102f882
Sizar Corpse <sizarcorpse@gmail.com>
2023-07-27 21:48:40 +0600

    fix(action): select id

--- src/actions/getTeamMembers.ts
+++ src/actions/getTeamMembers.ts
@@ -14,6 +14,7 @@ export default async function getTeamMembers() {
   try {
     const users = await prisma.user.findMany({
       select: {
+        id: true,
         image: true,
         username: true,
         role: true,
