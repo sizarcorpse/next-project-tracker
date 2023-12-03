c9d8acd73644d9c55d8b0efbbfe45709921bdac8
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-03 13:11:11 +0600

    wip(home-page): added ss fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,31 @@ import { CreateNewProject, ProjectsCollection } from "@/components/project";
 import { TagOverview } from "@/components/tag";
 import { TeamOverview } from "@/components/team";
 import { TechnologiesOverview } from "@/components/technologies";
-const Home = async () => {
+import { cookies } from "next/headers";
+
+async function getData() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("next-auth.session-token");
+
+  try {
+    const data = await fetch(`${process.env.NEXT_API_URL}/projects`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `next-auth.session-token=${token?.value}`,
+      },
+    });
+
+    const json = await data.json();
+    return json;
+  } catch (error) {
+    return [];
+  }
+}
+
+async function Home() {
+  const projects = await getData();
+
   return (
     <main className="flex flex-col gap-6 items-center justify-center">
       <div className="container max-w-screen-2xl mx-auto p-6 grid grid-cols-12 gap-4">
@@ -10,7 +34,7 @@ const Home = async () => {
           <div className="grid grid-cols-3">
             <CreateNewProject variant="card" />
           </div>
-          <ProjectsCollection />
+          <ProjectsCollection initialData={projects} />
         </div>
         <div className="col-span-12 md:col-span-4 grid grid-cols-1 content-start gap-6">
           <div className="">
@@ -26,6 +50,6 @@ const Home = async () => {
       </div>
     </main>
   );
-};
+}
 
 export default Home;
