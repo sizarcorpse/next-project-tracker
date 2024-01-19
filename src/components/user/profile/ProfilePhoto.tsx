2142b96ecc1a668bce75097594a61cd24fea2964
Sizar Corpse <sizarcorpse@gmail.com>
2024-01-19 09:08:44 +0600

    Add(user-comp): profile photo update functionality

--- src/components/user/profile/ProfilePhoto.tsx
+++ src/components/user/profile/ProfilePhoto.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { UpdateProfilePhoto } from "@/components/user/";
 import Image from "next/image";
 
 interface ProfilePhotoProps {
@@ -8,15 +9,20 @@ interface ProfilePhotoProps {
 
 const ProfilePhoto: React.FC<ProfilePhotoProps> = ({ image }) => {
   return (
-    <figure>
-      <Image
-        src={image || "/assets/images/default-avatar.jpeg"}
-        alt="Profile Photo"
-        width={296}
-        height={296}
-        className="rounded-full object-cover w-24 sm:w-32 md:w-72"
-      />
-    </figure>
+    <div className="group relative">
+      <figure>
+        <Image
+          src={image || "/assets/images/default-avatar.jpeg"}
+          alt="Profile Photo"
+          width={296}
+          height={296}
+          className="rounded-full aspect-square object-cover w-24 sm:w-32 md:w-72"
+        />
+      </figure>
+      <div className="hidden absolute bottom-4 left-0 right-0 group-hover:flex items-center justify-center">
+        <UpdateProfilePhoto />
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/user/profile/UpdateProfilePhoto.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { updateUserProfilePhoto } from "@/actions";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { UploadDropzone } from "@/utils/uploadthing";
+import { ImagePlus } from "lucide-react";
+import toast from "react-hot-toast";
+
+const UpdateProfilePhoto = () => {
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="ghost"
+            className="text-xs h-auto px-4 py-2 rounded-xl bg-muted text-muted-foreground shadow-sm"
+          >
+            Change
+            <ImagePlus className="ml-2 w-4 h-4" />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="bg-popover sm:max-w-screen-md min-h-fit flex flex-col items-start justify-start gap-6 p-4 sm:p-6 md:p-8 lg:p-12 xl:p-16 ">
+          <DialogHeader className="w-full">
+            <DialogTitle className="text-xl text-popover-foreground">
+              Upload Profile Photo
+            </DialogTitle>
+            <DialogDescription className="text-sm text-popover-foreground">
+              Upload a new Profile Photo here. The image should be a square with
+              a minimum size of 296x296 pixels. max. 4MB.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="w-full">
+            <UploadDropzone
+              className=" bg-primary-foreground rounded-xl w-full h-72"
+              endpoint="imageUploader"
+              content={{
+                uploadIcon: () => (
+                  <ImagePlus className="w-8 h-8" strokeWidth={1.75} />
+                ),
+                button({ ready, isUploading, uploadProgress }) {
+                  return (
+                    <div className="text-sm h-auto mt-2 px-4 py-2 rounded-xl bg-success text-success-foreground shadow-sm">
+                      {ready && !isUploading && !uploadProgress && "Upload"}
+                      {isUploading && !uploadProgress && "Uploading..."}
+                      {uploadProgress > 0 && `${uploadProgress}%`}
+                      {!ready &&
+                        !isUploading &&
+                        !uploadProgress &&
+                        "Getting ready..."}
+                    </div>
+                  );
+                },
+              }}
+              onClientUploadComplete={async (res) => {
+                try {
+                  const r = await updateUserProfilePhoto(res[0].url);
+                  if (r) {
+                    toast.success("Profile Photo updated!");
+                  }
+                } catch (error) {
+                  toast.error("Something went wrong!");
+                }
+              }}
+              onUploadError={(error: Error) => {
+                toast.error(error.message);
+              }}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default UpdateProfilePhoto;
