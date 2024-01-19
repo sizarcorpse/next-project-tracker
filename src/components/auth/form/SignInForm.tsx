5997c52824e20f0f57377d25a8468e154084c972
Sizar Corpse <sizarcorpse@gmail.com>
2024-01-19 06:52:11 +0600

    Update(auth-from): Update signin and signup from

--- src/components/auth/form/SignInForm.tsx
+++ src/components/auth/form/SignInForm.tsx
@@ -1,51 +1,60 @@
 "use client";
 
+import { navigate } from "@/actions/navigate";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { InputWithIcon } from "@/components/ui/input-with-icon";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
+// import {  useRouter } from "next/navigation";
+import {
+  userSigninValidation,
+  type UserSigninValidationRequest,
+} from "@/validators/user";
 import { useState } from "react";
-import { FieldErrors, FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-
-import { RiKeyLine, RiMailOpenLine } from "react-icons/ri";
 
 const SignInForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/";
 
-  const form = useForm<FieldValues>({
+  // const router = useRouter();
+
+  const form = useForm<UserSigninValidationRequest>({
+    resolver: zodResolver(userSigninValidation),
     defaultValues: {
       email: "",
       password: "",
     },
     mode: "onBlur",
   });
-
   const {
-    register,
     handleSubmit,
-    formState: { errors, isValid, isDirty, isSubmitting },
+    reset,
+    formState: { errors, isValid },
   } = form;
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (values: UserSigninValidationRequest) => {
     setIsLoading(true);
     try {
-      setIsLoading(true);
-
       const res = await signIn("credentials", {
         redirect: false,
-        email: data.email,
-        password: data.password,
-        callbackUrl,
+        email: values.email,
+        password: values.password,
       });
 
-      setIsLoading(false);
-
       if (!res?.error) {
+        setIsLoading(false);
         toast.success("Logged in");
-        router.refresh();
-        router.push(callbackUrl);
+        // await getSession();
+        // router.refresh();
+        navigate("/");
       } else {
         throw new Error(res.error);
       }
@@ -55,102 +64,65 @@ const SignInForm = () => {
     }
   };
 
-  const onError = (error: FieldErrors<FieldValues>) => {
-    console.log(error);
-  };
-
   return (
     <div className="w-full">
-      <form onSubmit={handleSubmit(onSubmit, onError)} noValidate>
-        <div className="flex flex-col gap-3">
-          <div className="w-full">
-            <div className="relative">
-              <RiMailOpenLine
-                className={`absolute top-1/2 left-4 transform -translate-y-1/2 ${
-                  errors.email?.message ? "text-destructive" : "text-primary"
-                }`}
-              />
-              <input
-                id="email"
-                autoComplete="email"
-                disabled={isLoading}
-                {...register("email", {
-                  required: {
-                    value: true,
-                    message: "Email is required",
-                  },
-                  pattern: {
-                    value:
-                      /^[a-zA-Z0-9._%+-]+@(gmail|outlook|hotmail|yahoo|proton)\.[a-zA-Z]{2,}$/i,
-                    message:
-                      "Email must be a valid email address. Only gmail, outlook, hotmail, yahoo and proton are allowed.",
-                  },
-                })}
-                placeholder="Email"
-                type="email"
-                className={`w-full px-10 py-4 bg-input border rounded-md outline-none text-sm text-primary font-normal disabled:opacity-70 disabled:cursor-not-allowed placeholder:font-normal ${
-                  errors.email?.message
-                    ? "border-destructive"
-                    : "border-transparent"
-                }`}
-              />
-            </div>
-            {errors.email?.message && (
-              <div className="text-destructive text-xs mt-3 pl-4">
-                {errors.email?.message as string}
-              </div>
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputWithIcon
+                    {...field}
+                    icon="Mail"
+                    placeholder="Email"
+                    className={`h-12 bg-primary-foreground/5 border-primary/80 text-primary-foreground ${
+                      errors.email
+                        ? "border-destructive focus-visible:ring-destructive"
+                        : "border-transparent"
+                    }`}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
             )}
-          </div>
-          <div className="w-full">
-            <div className="relative">
-              <RiKeyLine
-                className={`absolute top-1/2 left-4 transform -translate-y-1/2 ${
-                  errors.password?.message ? "text-destructive" : "text-primary"
-                }`}
-              />
-              <input
-                id="password"
-                disabled={isLoading}
-                {...register("password", {
-                  required: {
-                    value: true,
-                    message: "Password is required",
-                  },
-                  pattern: {
-                    value:
-                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                    message:
-                      "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character `tyUjs&6Y98`",
-                  },
-                })}
-                placeholder="Password"
-                autoComplete="current-password"
-                type="password"
-                className={`w-full px-10 py-4 bg-input border rounded-md outline-none text-sm text-primary font-normal disabled:opacity-70 disabled:cursor-not-allowed placeholder:font-normal ${
-                  errors.password?.message
-                    ? "border-destructive"
-                    : "border-transparent"
-                }`}
-              />
-            </div>
-            {errors.password?.message && (
-              <div className="text-destructive text-xs mt-3 pl-4">
-                {errors.password?.message as string}
-              </div>
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputWithIcon
+                    {...field}
+                    icon="KeyRound"
+                    placeholder="Password"
+                    type="password"
+                    className={`h-12 bg-primary-foreground/5 border-primary/80 text-primary-foreground ${
+                      errors.password
+                        ? "border-destructive focus-visible:ring-destructive"
+                        : "border-transparent"
+                    }`}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
             )}
-          </div>
-          <div className="w-full">
-            <button
-              disabled={!isDirty || !isValid || isSubmitting}
+          />
+          <div>
+            <Button
               type="submit"
-              title="Signup Button"
-              className="w-full py-4 px-8 text-center duration-500 bg-[length:200%_auto] text-primary-foreground rounded-md font-semibold shadow-sm bg-primary cursor-pointer select-none touch-manipulation hover:bg-accent hover:text-accent-foreground focus-ring disabled:bg-secondary disabled:text-secondary-foreground disabled:cursor-not-allowed disabled:touch-manipulation"
+              variant="success"
+              className="h-12 w-full"
+              disabled={isLoading || !isValid}
             >
               Continue
-            </button>
+            </Button>
           </div>
-        </div>
-      </form>
+        </form>
+      </Form>
     </div>
   );
 };
--- src/components/auth/form/SignUpForm.tsx
+++ src/components/auth/form/SignUpForm.tsx
@@ -1,41 +1,58 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { InputWithIcon } from "@/components/ui/input-with-icon";
+import {
+  userRegistrationValidation,
+  type UserRegistrationValidationRequest,
+} from "@/validators/user";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
-import { FieldErrors, FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-import {
-  RiKeyLine,
-  RiMailOpenLine,
-  RiUser3Line,
-  RiUser4Line,
-} from "react-icons/ri";
+
+// TODO: Check username availability.
 
 const SignUpForm = () => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<FieldValues>({
+  const form = useForm<UserRegistrationValidationRequest>({
+    resolver: zodResolver(userRegistrationValidation),
     defaultValues: {
       name: "",
       username: "",
       email: "",
       password: "",
+      passwordConfirmation: "",
     },
     mode: "onBlur",
   });
 
   const {
-    register,
     handleSubmit,
-    formState: { errors, isValid, isDirty, isSubmitting },
+    formState: { errors, isValid },
   } = form;
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (values: UserRegistrationValidationRequest) => {
     setIsLoading(true);
+
     try {
-      const response = await fetch("/api/signup", {
+      const response = await fetch("/api/auth/signup", {
         method: "POST",
-        body: JSON.stringify(data),
+        body: JSON.stringify({
+          name: values.name,
+          username: values.username,
+          email: values.email,
+          password: values.password,
+        }),
         headers: {
           "Content-Type": "application/json",
         },
@@ -48,8 +65,8 @@ const SignUpForm = () => {
       }
 
       await signIn("credentials", {
-        email: data.email,
-        password: data.password,
+        email: values.email,
+        password: values.password,
         callbackUrl: `/`,
       });
     } catch (error: any) {
@@ -58,181 +75,130 @@ const SignUpForm = () => {
     }
   };
 
-  const onError = (error: FieldErrors<FieldValues>) => {
-    console.log(error);
-  };
-
   return (
     <div className="w-full">
-      <form onSubmit={handleSubmit(onSubmit, onError)} noValidate>
-        <div className="flex flex-col gap-3">
-          <div className="w-full">
-            <div className="relative">
-              <RiUser4Line
-                className={`absolute top-1/2 left-4 transform -translate-y-1/2 ${
-                  errors.name?.message ? "text-destructive" : "text-primary"
-                }`}
-              />
-              <input
-                id="name"
-                disabled={isLoading}
-                {...register("name", {
-                  required: {
-                    value: true,
-                    message: "Name is required",
-                  },
-                  validate: {
-                    minLength: (value) => {
-                      if (value.length < 3) {
-                        return "Name must be at least 3 characters long";
-                      }
-                    },
-                  },
-                })}
-                placeholder="Name"
-                type="text"
-                className={`w-full px-10 py-4 bg-input border rounded-md outline-none text-sm text-primary font-normal disabled:opacity-70 disabled:cursor-not-allowed placeholder:font-normal ${
-                  errors.name?.message
-                    ? "border-destructive"
-                    : "border-transparent"
-                }`}
-              />
-            </div>
-          </div>
-          <div className="w-full">
-            <div className="relative">
-              <RiUser3Line
-                className={`absolute top-1/2 left-4 transform -translate-y-1/2 ${
-                  errors.username?.message ? "text-destructive" : "text-primary"
-                }`}
-              />
-              <input
-                id="username"
-                autoComplete="username"
-                disabled={isLoading}
-                {...register("username", {
-                  required: {
-                    value: true,
-                    message: "Username is required",
-                  },
-                  pattern: {
-                    value: /^[a-z0-9_]+$/,
-                    message: "Username must be alphanumeric",
-                  },
-                  validate: {
-                    isUsernameTaken: async (value) => {
-                      return true || "Username is already taken";
-                    },
-                  },
-                })}
-                placeholder="Username"
-                type="text"
-                className={`w-full px-10 py-4 bg-input border rounded-md outline-none text-sm text-primary font-normal disabled:opacity-70 disabled:cursor-not-allowed placeholder:font-normal ${
-                  errors.username?.message
-                    ? "border-destructive"
-                    : "border-transparent"
-                }`}
-              />
-            </div>
-            {errors.username?.message && (
-              <div className="text-destructive text-xs mt-3 pl-4">
-                {errors.username?.message as string}
-              </div>
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputWithIcon
+                    {...field}
+                    icon="Type"
+                    placeholder="Name"
+                    className={`h-12 bg-primary-foreground/5 border-primary/80 text-primary-foreground ${
+                      errors.name
+                        ? "border-destructive focus-visible:ring-destructive"
+                        : "border-transparent"
+                    }`}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
             )}
-          </div>
-          <div className="w-full">
-            <div className="relative">
-              <RiMailOpenLine
-                className={`absolute top-1/2 left-4 transform -translate-y-1/2 ${
-                  errors.email?.message ? "text-destructive" : "text-primary"
-                }`}
-              />
-              <input
-                id="email"
-                autoComplete="email"
-                disabled={isLoading}
-                {...register("email", {
-                  required: {
-                    value: true,
-                    message: "Email is required",
-                  },
-                  pattern: {
-                    value:
-                      /^[a-zA-Z0-9._%+-]+@(gmail|outlook|hotmail|yahoo|proton)\.[a-zA-Z]{2,}$/i,
-                    message:
-                      "Email must be a valid email address. Only gmail, outlook, hotmail, yahoo and proton are allowed.",
-                  },
-                  validate: {
-                    isEmailTaken: async (value) => {
-                      return true || "Email is already taken";
-                    },
-                  },
-                })}
-                placeholder="Email"
-                type="email"
-                className={`w-full px-10 py-4 bg-input border rounded-md outline-none text-sm text-primary font-normal disabled:opacity-70 disabled:cursor-not-allowed placeholder:font-normal ${
-                  errors.email?.message
-                    ? "border-destructive"
-                    : "border-transparent"
-                }`}
-              />
-            </div>
-            {errors.email?.message && (
-              <div className="text-destructive text-xs mt-3 pl-4">
-                {errors.email?.message as string}
-              </div>
+          />
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputWithIcon
+                    {...field}
+                    icon="User"
+                    placeholder="Username"
+                    className={`h-12 bg-primary-foreground/5 border-primary/80 text-primary-foreground ${
+                      errors.username
+                        ? "border-destructive focus-visible:ring-destructive"
+                        : "border-transparent"
+                    }`}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
             )}
-          </div>
-          <div className="w-full">
-            <div className="relative">
-              <RiKeyLine
-                className={`absolute top-1/2 left-4 transform -translate-y-1/2 ${
-                  errors.password?.message ? "text-destructive" : "text-primary"
-                }`}
-              />
-              <input
-                id="password"
-                disabled={isLoading}
-                {...register("password", {
-                  required: {
-                    value: true,
-                    message: "Password is required",
-                  },
-                  pattern: {
-                    value:
-                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                    message:
-                      "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character `tyUjs&6Y98`",
-                  },
-                })}
-                placeholder="Password"
-                autoComplete="current-password"
-                type="password"
-                className={`w-full px-10 py-4 bg-input border rounded-md outline-none text-sm text-primary font-normal disabled:opacity-70 disabled:cursor-not-allowed placeholder:font-normal ${
-                  errors.password?.message
-                    ? "border-destructive"
-                    : "border-transparent"
-                }`}
-              />
-            </div>
-            {errors.password?.message && (
-              <div className="text-destructive text-xs mt-3 pl-4">
-                {errors.password?.message as string}
-              </div>
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputWithIcon
+                    {...field}
+                    icon="Mail"
+                    placeholder="Email"
+                    className={`h-12 bg-primary-foreground/5 border-primary/80 text-primary-foreground ${
+                      errors.email
+                        ? "border-destructive focus-visible:ring-destructive"
+                        : "border-transparent"
+                    }`}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
             )}
-          </div>
-          <div className="w-full">
-            <button
-              disabled={!isDirty || !isValid || isSubmitting}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputWithIcon
+                    {...field}
+                    icon="KeyRound"
+                    placeholder="Password"
+                    type="password"
+                    className={`h-12 bg-primary-foreground/5 border-primary/80 text-primary-foreground ${
+                      errors.password
+                        ? "border-destructive focus-visible:ring-destructive"
+                        : "border-transparent"
+                    }`}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirmation"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <InputWithIcon
+                    {...field}
+                    icon="KeyRound"
+                    placeholder="Confirm password"
+                    type="password"
+                    className={`h-12 bg-primary-foreground/5 border-primary/80 text-primary-foreground ${
+                      errors.passwordConfirmation
+                        ? "border-destructive focus-visible:ring-destructive"
+                        : "border-transparent"
+                    }`}
+                  />
+                </FormControl>
+                <FormMessage className="text-xs font-light" />
+              </FormItem>
+            )}
+          />
+          <div>
+            <Button
               type="submit"
-              title="Signup Button"
-              className="w-full py-4 px-8 text-center duration-500 bg-[length:200%_auto] text-primary-foreground rounded-md font-semibold shadow-sm bg-primary cursor-pointer select-none touch-manipulation hover:bg-accent hover:text-accent-foreground focus-ring disabled:bg-secondary disabled:text-secondary-foreground disabled:cursor-not-allowed disabled:touch-manipulation"
+              variant="success"
+              className="h-12 w-full"
+              disabled={isLoading || !isValid}
             >
               Continue
-            </button>
+            </Button>
           </div>
-        </div>
-      </form>
+        </form>
+      </Form>
     </div>
   );
 };
