ed1060d0b198cc193b678eee02db4a63637071c9
Sizar Corpse <sizarcorpse@gmail.com>
2024-01-24 15:42:23 +0600

    update pages and components

--- src/components/projects/ProjectsToolbar.tsx
+++ src/components/projects/ProjectsToolbar.tsx
@@ -4,18 +4,25 @@ import {
   ProjectsSort,
   ProjectsStatus,
 } from "@/components/projects";
+import { Suspense } from "react";
+
+function SearchBarFallback() {
+  return null;
+}
 
 const ProjectsToolbar = () => {
   return (
     <div className="flex flex-col justify-between w-full sm:flex-row">
-      <div className="flex gap-2">
-        <ProjectsSignature />
-        <ProjectsStatus />
-      </div>
-      <div className="flex gap-2">
-        <ProjectsFilter />
-        <ProjectsSort />
-      </div>
+      <Suspense fallback={<SearchBarFallback />}>
+        <div className="flex gap-2">
+          <ProjectsSignature />
+          <ProjectsStatus />
+        </div>
+        <div className="flex gap-2">
+          <ProjectsFilter />
+          <ProjectsSort />
+        </div>
+      </Suspense>
     </div>
   );
 };
