228ee3add290f73e040f37ee75d6006d0a1252d7
Sizar Corpse <sizarcorpse@gmail.com>
2024-01-24 15:57:06 +0600

    add indexes

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,4 @@
 export { default as PTDialog } from "./PTDialog";
 export { default as Svg } from "./Svg";
+export { default as Unauthenticated } from "./Unauthenticated";
+export { default as Unauthorized } from "./Unauthorized";
