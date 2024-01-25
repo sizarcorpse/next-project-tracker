25de7f3bc8bd08827adb788d40d1d10530e5baea
Sizar Corpse <sizarcorpse@gmail.com>
2024-01-25 11:20:39 +0600

    Update(project-comp): update CreateProjectForm, add action, refactor

--- src/components/project/CreateProjectForm.tsx
+++ src/components/project/CreateProjectForm.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-
 import {
   Form,
   FormControl,
@@ -10,34 +8,44 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { CREATE_PROJECT_PERMISSION } from "@/utils/permissions";
+import { Loader2 } from "lucide-react";
 
+import { createProject } from "@/actions/project/createProject";
 import {
   ProjectCurrentStage,
   ProjectPriority,
+  ProjectRedirect,
   ProjectType,
   ProjectVisibility,
 } from "@/components/project/";
 import { Input } from "@/components/ui/input";
+import { usePermission } from "@/hooks/";
+import { cn } from "@/libs/utils";
+import {
+  createProjectValidation,
+  type CreateProjectRequest,
+} from "@/validators/project";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { FC, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-import * as z from "zod";
-
-const projectSchema = z.object({
-  title: z.string().min(3, "Name should be at least 3 characters"),
-  type: z.string().optional(),
-  priority: z.string().optional(),
-  visibility: z.string().optional(),
-  stage: z.string().optional(),
-});
 
 const CreateProjectForm = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isRedirect, setRedirect] = useState<boolean>(true);
   const router = useRouter();
-  const form = useForm<z.infer<typeof projectSchema>>({
-    resolver: zodResolver(projectSchema),
+
+  const { data: session } = useSession();
+  const isPermitted = usePermission(session?.user?.permissions, [
+    CREATE_PROJECT_PERMISSION,
+  ]);
+
+  const form = useForm<CreateProjectRequest>({
+    resolver: zodResolver(createProjectValidation),
     defaultValues: {
       title: "",
       type: undefined,
@@ -56,26 +64,24 @@ const CreateProjectForm = () => {
   const onSubmit = async (data: any) => {
     try {
       setIsLoading(true);
-      const response = await fetch(`${process.env.NEXT_API_URL}/projects`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: data.title,
-          type: data.type,
-          priority: data.priority,
-          visibility: data.visibility,
-          stage: data.stage,
-        }),
-      });
-      const json = await response.json();
-      if (!response.ok) {
-        throw new Error(json.message);
+      if (!isPermitted) {
+        throw Error("You don't have permission to create a project.");
       }
-      setIsLoading(false);
-      toast.success("Technology created successfully!");
-      router.push(`/projects/${json.data.slug}/update`);
+
+      const res = await createProject(data);
+      if (res.status !== "ok") {
+        throw Error(res.message);
+      }
+
+      toast.success("Project created successfully ✨");
+
+      if (isRedirect) {
+        setIsLoading(false);
+        router.push(`/projects/${res.data?.slug}/update`);
+      } else {
+        reset();
+        setIsLoading(false);
+      }
     } catch (error: any) {
       toast.error(error.message);
       setIsLoading(false);
@@ -83,11 +89,11 @@ const CreateProjectForm = () => {
   };
 
   return (
-    <div className="w-full">
+    <div className="w-full space-y-8">
       <Form {...form}>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
           <div className="flex flex-col">
-            <h2 className="text-4xl text-popover-foreground">
+            <h2 className="text-2xl font-semibold text-popover-foreground md:text-4xl ">
               Lets start with a name for <br />
               your project.
             </h2>
@@ -120,15 +126,33 @@ const CreateProjectForm = () => {
             <ProjectCurrentStage form={form} />
           </div>
           <Separator />
-          <Button
-            type="submit"
-            disabled={!isValid || isLoading}
-            className="mt-auto"
-          >
-            {isLoading ? "Loading..." : "Create Project"}
-          </Button>
+          <div className="flex flex-col gap-4 items-center justify-between sm:flex-row">
+            <Button
+              type="submit"
+              disabled={!isValid || isLoading}
+              className={cn(`mt-auto gap-2`)}
+            >
+              <Loader2
+                strokeWidth={1.5}
+                className={cn(
+                  `hidden w-4 h-4`,
+                  isLoading && `block animate-spin`
+                )}
+              />
+              {isLoading ? "Creating Project" : "Create Project"}
+            </Button>
+            <ProjectRedirect setRedirect={setRedirect} value={isRedirect} />
+          </div>
         </form>
       </Form>
+      <div>
+        {!isPermitted && (
+          <div className="text-center text-base text-destructive">
+            You don&apos;t have permission to create a project. Contact your
+            administrator to get access.
+          </div>
+        )}
+      </div>
     </div>
   );
 };
